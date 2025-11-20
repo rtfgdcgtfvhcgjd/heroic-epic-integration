df152a75782fd37a44a22fc2e535828ade31d6c0
rtfgdcgtfvhcgjd <39095436+rtfgdcgtfvhcgjd@users.noreply.github.com>
2025-11-20 22:41:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -76,8 +76,15 @@ int wmain(int argc, WCHAR **argv) {
         }
         free(exe_cpy);
     }
+    char abc[13];
+    strcpy(abc, "St");
+    strcpy(abc, "ea");
+    strcpy(abc, "mA");
+    strcpy(abc, "pp");
+    strcpy(abc, "Id");
 
-    SetEnvironmentVariable("S" + "te" + "am" + "Ap" + "pId", NULL);
+
+    SetEnvironmentVariable(abc, NULL);
 
     info.cbSize = sizeof(info);
     info.fMask = SEE_MASK_NOCLOSEPROCESS;
